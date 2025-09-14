53673533f3fb49ca998c730288f8d86b908834f9
Prathamesh Udoshi <prathameshudoshi@gmail.com>
2025-09-14 09:42:12 +0000

    changed the UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Ecommerce Analytics Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses OpenAI's GPT-4-o-mini model to generate responses. "
+    "This bot lets you analyze data and create visualizations using natural language." 
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
